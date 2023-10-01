7ea8b1325dfb1924feaa95c61b6d2d8cbbae420a
SuperShySuperShy <141748434+SuperShySuperShy@users.noreply.github.com>
2023-10-01 10:08:12 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -10,36 +10,44 @@ class App extends Component {
     };
   }
 
-  
+  handleClick = (value) => {
+    if (value === '=') {
+      try {
+        this.setState({ result: eval(this.state.input) });
+      } catch (error) {
+        this.setState({ result: "Error" });
+      }
+    } else if (value === 'C') {
+      this.setState({ input: "", result: "" });
+    } else {
+      this.setState({ input: this.state.input + value });
+    }
+  };
+
   render() {
     return (
       <div className="calculator">
-        <div className="header">
-          <h2>Welcome to My Calculator App</h2>
-        </div>
         <div className="display">
           <input type="text" value={this.state.input} readOnly />
           <div className="result">{this.state.result}</div>
         </div>
         <div className="buttons">
-          <button onClick={() => this.handleButtonClick('7')}>7</button>
-          <button onClick={() => this.handleButtonClick('8')}>8</button>
-          <button onClick={() => this.handleButtonClick('9')}>9</button>
-          <button onClick={() => this.handleButtonClick('+')}>+</button>
-          <button onClick={() => this.handleButtonClick('4')}>4</button>
-          <button onClick={() => this.handleButtonClick('5')}>5</button>
-          <button onClick={() => this.handleButtonClick('6')}>6</button>
-          <button onClick={() => this.handleButtonClick('-')}>-</button>
-          <button onClick={() => this.handleButtonClick('1')}>1</button>
-          <button onClick={() => this.handleButtonClick('2')}>2</button>
-          <button onClick={() => this.handleButtonClick('3')}>3</button>
-          <button onClick={() => this.handleButtonClick('*')}>*</button>
-          <button onClick={() => this.handleButtonClick('C')}>C</button>
-          <button onClick={() => this.handleButtonClick('0')}>0</button>
-          <button onClick={() => this.handleButtonClick('=')}>=</button>
-          <button onClick={() => this.handleButtonClick('/')}>/</button>
-          <button onClick={() => this.handleButtonClick('⌫')}>⌫</button>
-          <button onClick={() => this.handleButtonClick('00')}>00</button>
+          <button onClick={() => this.handleClick('7')}>7</button>
+          <button onClick={() => this.handleClick('8')}>8</button>
+          <button onClick={() => this.handleClick('9')}>9</button>
+          <button onClick={() => this.handleClick('+')}>+</button>
+          <button onClick={() => this.handleClick('4')}>4</button>
+          <button onClick={() => this.handleClick('5')}>5</button>
+          <button onClick={() => this.handleClick('6')}>6</button>
+          <button onClick={() => this.handleClick('-')}>-</button>
+          <button onClick={() => this.handleClick('1')}>1</button>
+          <button onClick={() => this.handleClick('2')}>2</button>
+          <button onClick={() => this.handleClick('3')}>3</button>
+          <button onClick={() => this.handleClick('*')}>*</button>
+          <button onClick={() => this.handleClick('C')}>C</button>
+          <button onClick={() => this.handleClick('0')}>0</button>
+          <button onClick={() => this.handleClick('=')}>=</button>
+          <button onClick={() => this.handleClick('/')}>/</button>
         </div>
       </div>
     );
